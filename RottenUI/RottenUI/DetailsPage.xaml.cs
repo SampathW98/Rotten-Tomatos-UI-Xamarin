c59a273f6630e2639fca33cdb9a6d352c1a146b6
Sampath Warakagoda <sampathchanaka.bandara@gmail.com>
2023-04-08 19:09:09 +0530

    DetailsPage finished

--- RottenUI/RottenUI/DetailsPage.xaml.cs
+++ RottenUI/RottenUI/DetailsPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using RottenUI.Model;
 using RottenUI.ViewModel;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +23,34 @@ namespace RottenUI
             //Title = myMovie.Title;
 			
 		}
+        /*
+        private void SKCanvasView_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs args)
+        {
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
 
+            canvas.Clear();
+
+            using (SKPaint paint = new SKPaint())
+            {
+                // define the color for the shadow
+                SKColor shadowColor = Color.FromHex("#5ACB6E").ToSKColor();
+
+                paint.IsDither = true;
+                paint.IsAntialias = true;
+                paint.Color = shadowColor;
+
+                // create filter for drop shadow
+                paint.ImageFilter = SKImageFilter.CreateDropShadow(
+                    dx: 0, dy: 0, sigmaX: 40, sigmaY: 40, color: shadowColor, shadowMode: SKDropShadowImageFilterShadowMode.DrawShadowOnly);
+
+
+                    //dx: 0, dy: 0,
+                    //sigmaX: 40, sigmaY: 40,
+                    //color: shadowColor,
+                    //shadowMode: SKDropShadowImageFilterShadowMode.DrawShadowOnly);
+            }
+        }*/
     }
 }
---
+++ RottenUI/RottenUI/Views/DetailsSection.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RottenUI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailsSection : ContentView
+    {
+        int selectionIndex = 0;
+        List<Label> tabHeaders = new List<Label>();
+        List<VisualElement> tabContent = new List<VisualElement>();
+
+        public DetailsSection()
+        {
+            InitializeComponent();
+
+            tabHeaders.Add(InfoTab);
+            tabHeaders.Add(CastTab);
+            tabHeaders.Add(NewsTab);
+            tabHeaders.Add(CriticsTab);
+            tabHeaders.Add(MediaTab);
+
+            tabContent.Add(InfoContent);
+            tabContent.Add(CastContent);
+            tabContent.Add(NewsContent);
+            tabContent.Add(CriticsContent);
+            tabContent.Add(MediaContent);
+        }
+
+        private async Task ShowSelection(int newTab)
+        {
+            if(newTab == selectionIndex) { return; }
+
+            //navigate the selection pill
+            var selectdTabLabel = tabHeaders[newTab];
+            _ = SelectionUnderline.TranslateTo(selectdTabLabel.Bounds.X, 0, 150, easing:Easing.SinInOut);
+
+            //update style of header
+            var unselectedStyle = (Style)Application.Current.Resources["TabLabel"];
+            var selectedStyle = (Style)Application.Current.Resources["SelectedTabLabel"];
+
+            tabHeaders[selectionIndex].Style = unselectedStyle;
+            selectdTabLabel.Style = selectedStyle;
+
+            //change the actual tabs
+            await tabContent[selectionIndex].FadeTo(0);
+            tabContent[selectionIndex].IsVisible = false;
+            tabContent[newTab].IsVisible = true;
+            _ = tabContent[selectionIndex].FadeTo(1);
+
+            selectionIndex = newTab;
+        }
+         
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var tabIndex = tabHeaders.IndexOf((Label)sender);
+            await ShowSelection(tabIndex);
+        }
+    }
+}
